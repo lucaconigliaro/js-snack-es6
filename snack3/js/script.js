769fa934cc9a61eb4345257bdb2fd6c43b567ceb
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-12 18:52:31 +0100

    snack3 bonus

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,3 +1,27 @@
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS
+
+//Funzione in cui dichiaro un nuovo array che conterrà i numeri compresi 
+//tra un minimo ed un massimo specificati
+function myFunction(array, min, max) {
+    newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const curArray = array [i];
+        if(curArray >= a && curArray <= b){
+            newArray.push(curArray);
+        }
+    }
+    return newArray;
+}
+
+//Array con diversi numeri
+const array0 = [13, 22, 3, 78, 9,];
+
+//Intervallo di valori 
+const a = 1;
+const b = 20;
+
+//La funzione restituisce un nuovo array con i numeri di array0 che sono compresi tra 1 e 20 
+console.log(myFunction(array0, a, b));
